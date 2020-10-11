336c7eee617ccbe9378ab74749d23334a4c3ac80
ihar-dambrouski <60747836+ihar-dambrouski@users.noreply.github.com>
2020-10-11 19:12:55 +0300

    simple calculator

--- app/src/main/java/com/example/viewapplication/MainActivity.java
+++ app/src/main/java/com/example/viewapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.os.Bundle;
 
@@ -11,47 +12,104 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    int clicks = 0;
+    TextView resultField;
+    EditText numberField;
+    TextView operationdField;
+    Double operand = null;
+    String lastOperation = "=";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View topButtonPanel = findViewById(R.id.top_button_panel);
-        View bottomButtonPanel = findViewById(R.id.bottom_button_panel);
-        final TextView clicksText = findViewById(R.id.clicksText);
-
-        Button topButton = topButtonPanel.findViewById(R.id.clickBtn);
-        Button bottomButton = bottomButtonPanel.findViewById(R.id.clickBtn);
-
-
-        topButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setClicksCount(false, clicksText);
-            }
-        });
-
-        bottomButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setClicksCount(true, clicksText);
-            }
-        });
-
-
+        resultField = (TextView) findViewById(R.id.resultField);
+        numberField = (EditText) findViewById(R.id.numberField);
+        operationdField = (TextView) findViewById(R.id.operationField);
     }
 
-    public void setClicksCount(boolean isDecriment, TextView textView) {
-        System.out.println(isDecriment);
-        if(isDecriment) {
-            clicks--;
-        } else {
-            clicks++;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand != null) {
+            outState.putDouble("OPERAND", operand);
         }
-        System.out.println(clicks + "");
-        textView.setText(clicks + " Clicks");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        operand = savedInstanceState.getDouble("OPERAND");
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operationdField.setText(lastOperation);
+        resultField.setText(operand.toString());
+    }
+
+
+    public void  onNumberClick(View v) {
+        Button button = (Button)v;
+        numberField.append(button.getText());
+
+        if(lastOperation.equals('=') && operand != null) {
+            operand = null;
+        }
+    }
+
+    public void onOperationClick(View v) {
+        Button button = (Button) v;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+
+        if(number.length() > 0) {
+            number = number.replace(',', '.');
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationdField.setText(lastOperation);
+    }
+
+    public void performOperation(Double number, String operation) {
+
+        // если операнд ранее не был установлен (при вводе самой первой операции)
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
 
     }
 
